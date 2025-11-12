bf4002b2b27b2156010eeefdcaec6891c9f45cca
mike <h@solfunmeme.com>
2025-11-12 20:37:43 +0000

    feat: Add full-flake.nix to the repository

---
+++ full-flake.nix
@@ -0,0 +1,255 @@
+{
+  inputs = {
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    rust-overlay = {
+      url = "github:meta-introspector/rust-overlay?ref=feature/CRQ-016-nixify";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
+    flake-compat = {
+      url = "github:meta-introspector/flake-compat?ref=feature/CRQ-016-nixify";
+      flake = false;
+    };
+    context = {
+      url = "github:meta-introspector/streamofrandom?ref=feature/foaf&dir=2025/10/10";
+      flake = false;
+    };
+    allocator-api2.url = "github:meta-introspector/allocator-api2?ref=feature/CRQ-016-nixify";
+    #    time-rs.url = "github:meta-introspector/time-rs?ref=feature/CRQ-016-nixify";
+  };
+
+  outputs = inputs: with inputs;
+    let
+      overlays = import ./overlay rust-overlay.overlays.default;
+      combinedOverlay = overlays.combined;
+
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          # 1. Setup nixpkgs with rust and cargo2nix overlays.
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              combinedOverlay
+            ];
+            config = {
+              permittedInsecurePackages = [ "openssl-1.1.1w" ];
+            };
+          };
+
+          #rustToolchain = pkgs.rust-bin.stable."1.81.0".default;
+          #rustToolchain = pkgs.rust-bin.nightly."2025-10-06".default;
+          #rustToolchain = pkgs.rust-bin.nightly."2025-10-08".default;
+          #rustToolchain = pkgs.rust-bin.nightly."2025-10-16".default;          
+          rustToolchain = pkgs.rust-bin.nightly."2025-09-16".default;
+
+          # 2. Builds the rust package set, which contains all crates in your cargo workspace's dependency graph.
+          # `makePackageSet` accepts the following arguments:
+          # - `packageFun` (required): The generated `Cargo.nix` file, which returns the whole dependency graph.
+          # - `ignoreLockHash` (optional): Set to `true` to turn off the hash check between Cargo.lock and Cargo.nix.
+          # - `workspaceSrc` (optional): Sources for the workspace can be provided or default to the current directory.
+          # You must set xsome combination of `rustChannel` + `rustVersion` or `rustToolchain`.
+          # - `rustToolchain` (optional): Completely override the toolchain.  Must provide rustc, cargo, rust-std, and rust-src components
+          # - `rustChannel` (optional): "nightly" "stable" "beta".  To support legacy use, this can be a version when supplied alone.  If unspecified, defaults to "stable".
+          # - `rustVersion` (optional): "1.75.0" "2023-12-30".  If not supplied, "latest" will be assumed.
+          # - `rustProfile` (optional): "minimal" or "default" usually.  "minimal" if not specified (for faster builds)
+          # - `extraRustComponents` (optional): ["rustfmt" "clippy"].
+          # - `packageOverrides` (optional):
+          #     A function taking a package set and returning a list of overrides.
+          #     Overrides are introduced to provide native inputs to build the crates generated in `Cargo.nix`.
+          #     See `overlay/lib/overrides.nix` on how to create overrides and `overlay/overrides.nix` for a list of predefined overrides.
+          #     Most of the time, you can just use `overrides.all`. You can hand-pick overrides later if your build becomes too slow.
+          # - `rootFeatures` (optional):
+          #     A list of activated features on your workspace's crates.
+          #     Each feature should be of the form `<crate_name>[/<feature>]`.
+          #     If `/<feature>` is omitted, the crate is activated with no default features.
+          #     The default behavior is to activate all crates with default features.
+          # - `fetchCrateAlternativeRegistry` (optional): A fetcher for crates on alternative registries.
+          # - `release` (optional): Whether to enable release mode (equivalent to `cargo build --release`), defaults to `true`.
+          # - `hostPlatformCpu` (optional):
+          #     Equivalent to rust's target-cpu codegen option. If specified "-Ctarget-cpu=<value>" will be added to the set of rust
+          #     flags used for compilation of the package set.
+          # - `hostPlatformFeatures` (optional):
+          #     Equivalent to rust's target-feature codegen option. If specified "-Ctarget-feature=<values>" will be added to the set of rust
+          #     flags used for compilation of the package set. The value should be a list of the features to be turned on, without the leading "+",
+          #     e.g. `[ "aes" "sse2" "ssse3" "sse4.1" ]`.  They will be prefixed with a "+", and comma delimited before passing through to rust.
+          #     Crates that check for CPU features such as the `aes` crate will be evaluated against this argument.
+          #    cargoUnstableFlags (optional):
+          #     Passes "-Z" flags that affect cargo unstable features.  Example [ "build-std=panic_abort" "std" ]
+          #    rustcLinkFlags (optional):
+          #     Pass extra flags directly to rustc during non-build invocations
+          #    rustcBuildFlags (optional):
+          #     Pass extra flags directly to Rustc during build invocations
+          # - `target` (optional):
+          #     Set an explicit Rust output target.  Overrides the translation
+          #     from Nix targets to Rust targets.  See overlay/lib/rust-triple.nix
+          #     for more info.
+          # - `targetSpecFile`, `targetSpec` (optional):
+          #     This is used to resolve certain conditional package dependencies for platforms that `cargo2nix` does not know about.
+          #     Most users do not need this, even when cross-compiling.
+          #     If you are already passing a target spec file to `target`, this will be filled in for you automatically.
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            packageFun = import ./Cargo.nix;
+            rustChannel = "nightly";
+            rustVersion = "latest";
+
+            #inherit rustToolchain;
+            #workspaceSrc = self;
+            #rootFeatures = [ "time-macros/large-dates" ];
+            packageOverrides = pkgs: [
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "heapless";
+                overrideAttrs = old: {
+                  rustcBuildFlags = (old.rustcBuildFlags or [ ]) ++ [ "--allow=warnings" "--allow=dead_code" ];
+                };
+              })
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "allocator-api2";
+                overrideAttrs = old: {
+                  src = allocator-api2;
+                };
+              })
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "time-macros";
+                overrideAttrs = old: {
+                  features = [ "large-dates" "formatting" "parsing" "serde" ];
+                };
+              })
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "time";
+                overrideAttrs = old: {
+                  features = [ "std" "alloc" "formatting" "parsing" "serde" "local-offset" "wasm-bindgen" ];
+                };
+              })
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "time";
+                overrideAttrs = old: {
+                  features = [ "std" "alloc" "formatting" "parsing" "serde" "local-offset" "wasm-bindgen" ];
+                };
+              })
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "cargo";
+                overrideAttrs = old:
+                  let
+                    extraCargoBuildFlags = old.extraCargoBuildFlags or [ ];
+                  in
+                  {
+                    runCargo = ''
+                      (
+                        set -euo pipefail
+                        if (( NIX_DEBUG >= 1 )); then
+                          set -x
+                        fi
+                        env \
+                          "CC_aarch64-unknown-linux-gnu"="${pkgs.gcc}/bin/cc" \
+                          "CXX_aarch64-unknown-linux-gnu"="${pkgs.gcc}/bin/c++" \
+                          ${rustToolchain}/bin/cargo build $CARGO_VERBOSE --release --target aarch64-unknown-linux-gnu \
+                          ${pkgs.lib.strings.concatStringsSep " " extraCargoBuildFlags} \
+                        --message-format json-diagnostic-rendered-ansi | tee .cargo-build-output \
+                        1> >(jq 'select(.message != null) .message.rendered' -r)
+                      )
+                    '';
+                  };
+              })
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "openssl-sys";
+                overrideAttrs = old: {
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.pkg-config pkgs.openssl ];
+                };
+              })
+            ];
+          };
+          # `rustPkgs` now contains all crates in the dependency graph.
+          # To build normal binaries, use `rustPkgs.<registry>.<crate>.<version> { }`.
+          # To build test binaries (equivalent to `cargo build --tests`), use
+          #   `rustPkgs.<registry>.<crate>.<version>{ compileMode = "test"; }`.
+          # To build bench binaries (equivalent to `cargo build --benches`), use
+          #   `rustPkgs.<registry>.<crate>.<version>{ compileMode = "bench"; }`.
+          # For convenience, you can also refer to the crates in the workspace using
+          #   `rustPkgs.workspace.<crate>`.
+          #
+          # When a crate is not associated with any registry, such as when building
+          # locally, the registry is "unknown" as shown below:
+          # rustPkgs.unknown.cargo2nix."0.12.0"
+          # An example of a crates.io path:
+          # rustPkgs."registry+https://github.com/rust-lang/crates.io-index".openssl."0.10.30"
+
+          cargo2nix = rustPkgs.workspace.cargo2nix { }; # supports override & overrideAttrs
+
+          # The workspace defines a development shell with all of the dependencies
+          # and environment settings necessary for a regular `cargo build`.
+          # Passes through all arguments to pkgs.mkShell for adding supplemental
+          # dependencies.
+          workspaceShell = rustPkgs.workspaceShell {
+            packages = [ pkgs.statix pkgs.openssl_1_1.dev ];
+            shellHook = ''
+              export PKG_CONFIG_PATH=${pkgs.openssl_1_1.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+              export PATH=${rustToolchain}/bin:$PATH
+            '';
+          }; # supports override & overrideAttrs
+
+          # A shell for users to quickly bootstrap projects.  Contains cargo2nix
+          # and the rustToolchain used to build this cargo2nix.
+          bootstrapShell = pkgs.mkShell {
+            packages = [ cargo2nix ];
+            # inputsFrom = [ cargo2nix ];
+            inherit (cargo2nix) nativeBuildInputs;
+          };
+
+        in
+        rec {
+
+          devShells = {
+            # nix develop
+            default = workspaceShell;
+            # nix develop .#bootstrap
+            bootstrap = bootstrapShell;
+          };
+
+          packages = rec {
+            # nix build .#packages.x86_64-linux.cargo2nix
+            inherit cargo2nix;
+            # nix build .#cargo2nix
+            default = cargo2nix;
+
+            timeMacros = rustPkgs.workspace.time-macros { };
+
+            # `runTests` runs all tests for a crate inside a Nix derivation.  This
+            # may be problematic as Nix may restrict filesystem, network access,
+            # socket creation, which the test binary may need.
+            # If you run to those problems, build test binaries (as shown above in
+            # workspace derivation arguments) and run them manually outside a Nix
+            # derivation.s
+            ci = pkgs.rustBuilder.runTests rustPkgs.workspace.cargo2nix {
+              /* Add `depsBuildBuild` test-only deps here, if any. */
+            };
+
+            # for legacy users
+            shell = devShells.default;
+          };
+
+          apps = rec {
+            # nix run .#cargo2nix
+            # nix run github:cargo2nix/cargo2nix
+            cargo2nix = { type = "app"; program = "${packages.default}/bin/cargo2nix"; };
+            # nix run
+            # nix run github:cargo2nix/cargo2nix
+            default = cargo2nix;
+          };
+        }
+      ) // {
+      # The above outputs are mapped over system for `nix run` and `nix develop`
+      # workflows.  They are merged with these system-independent attributes,
+      # which are top level attributes can be used directly in downstream
+      # flakes.  If `cargo2nix` is your flake input, `cargo2nix.overlay` is the
+      # overlay.
+      inherit overlays;
+      # Nix flake check complains.  I will keep this attribute alive until next
+      # version branch-off.
+      overlay = builtins.trace
+        "cargo2nix.overlay is deprecated.  Use cargo2nix.overlays.default"
+        overlays.default;
+      crq = "016";
+    };
+}
